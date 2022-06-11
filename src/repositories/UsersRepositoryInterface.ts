4e7a6476500f097ee7549aa1346ba19a7373a506
gerssivaldosantos <canalandows@gmail.com>
2022-06-10 23:48:23 -0300

    feat: add user repository interface

---
+++ src/repositories/UsersRepositoryInterface.ts
@@ -0,0 +1,10 @@
+import { User } from "../entities/User";
+
+export interface UsersRepositoryInterface {
+    create(user: User): Promise<void>;
+    findById(id: string): Promise<User | undefined>;
+    findByEmail(email: string): Promise<User | undefined>;
+    findAll(): Promise<User[]>;
+    update(user: User): Promise<User>;
+    delete(id: string): Promise<void>;
+}
