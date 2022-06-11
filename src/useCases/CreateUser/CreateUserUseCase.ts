9f60b099672cb576c945e316e0167b40cd6f4c58
gerssivaldosantos <canalandows@gmail.com>
2022-06-11 00:19:12 -0300

    refactor: instance the private attribute class directly by private command in constructor paramater

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,13 +1,9 @@
 import { UsersRepositoryInterface } from "../../repositories/UsersRepositoryInterface";
 
 export class CreateUserUseCase {
-    
-    private usersRepository: UsersRepositoryInterface
-
     constructor(
-        usersRepository: UsersRepositoryInterface
+        private usersRepository: UsersRepositoryInterface
     ){}
     async execute () {
-        this.usersRepository = this.usersRepository
     }
 }
