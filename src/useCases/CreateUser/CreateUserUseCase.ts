ac1cfcc343b8d5a026ff1ecefcdb4f600c8d82b7
gerssivaldosantos <canalandows@gmail.com>
2022-06-11 00:37:35 -0300

    feat: add create user implementation

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,9 +1,18 @@
+import { User } from "../../entities/User";
 import { UsersRepositoryInterface } from "../../repositories/UsersRepositoryInterface";
+import { CreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
     constructor(
         private usersRepository: UsersRepositoryInterface
     ){}
-    async execute () {
+    async execute (data: CreateUserRequestDTO) {
+       const userSearched = await this.usersRepository.findByEmail(data.email)
+       
+       if (userSearched){
+           throw new Error("User already exists")
+       }
+        const user = new User(data)
+        await this.usersRepository.save(user)
     }
 }
