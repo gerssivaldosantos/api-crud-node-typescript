81c8ab1047735cee8e9f39dfd6bafe50ee243e6f
gerssivaldosantos <canalandows@gmail.com>
2022-06-11 13:16:33 -0300

    feat: add create users table migration

---
+++ src/databases/typeorm/migrations/1654963954243-createUsersTable.ts
@@ -0,0 +1,49 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createUsersTable1654963954243 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        isNullable: false
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        isNullable: false
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users')
+    }
+
+}
