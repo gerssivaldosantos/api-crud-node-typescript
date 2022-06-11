5f8f29e6dca06cf152779b78c05a284ff82f998d
gerssivaldosantos <canalandows@gmail.com>
2022-06-11 12:15:23 -0300

    feat: add typeorm user repository basic structure

---
+++ src/repositories/implementations/TypeormUsersRepository.ts
@@ -0,0 +1,25 @@
+import { Repository } from "typeorm";
+import { User } from "../../entities/User";
+import { UsersRepositoryInterface } from "../UsersRepositoryInterface";
+
+export class TypeormUsersRepository implements UsersRepositoryInterface {
+    constructor (private readonly usersRepository: Repository<User>){}
+    save(user: User): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    findById(id: string): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+    findByEmail(email: string): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<User[]> {
+        throw new Error("Method not implemented.");
+    }
+    update(user: User): Promise<User> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+}
