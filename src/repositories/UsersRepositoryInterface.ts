ca1ceae5bb32a65502b64be5461079ac40bbaf8a
gerssivaldosantos <canalandows@gmail.com>
2022-06-11 00:38:01 -0300

    refactor: change the "create" to "save" (name)

--- src/repositories/UsersRepositoryInterface.ts
+++ src/repositories/UsersRepositoryInterface.ts
@@ -1,7 +1,7 @@
 import { User } from "../entities/User";
 
 export interface UsersRepositoryInterface {
-    create(user: User): Promise<void>;
+    save(user: User): Promise<void>;
     findById(id: string): Promise<User | undefined>;
     findByEmail(email: string): Promise<User | undefined>;
     findAll(): Promise<User[]>;
