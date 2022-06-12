fd7238880d751e338db02a3e737ba655ed5911a2
gerssivaldosantos <canalandows@gmail.com>
2022-06-12 13:03:49 -0300

    refactor: remove prefix for routes

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ class MyApp {
         this.app.use(express.json());
         this.app.use(cors());
         // this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-        this.app.use(process.env.SULFIX_URL, router)
+        this.app.use(router)
     }
 }
 
