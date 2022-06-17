76ca852899d49fc73ca8b5d17dde2b702cd403c7
gerssivaldosantos <canalandows@gmail.com>
2022-06-17 01:23:21 -0300

    feat: add login route

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express"
 import { createUserController } from "./useCases/CreateUser"
 import { deleteUserController } from "./useCases/DeleteUserById"
 import { getUserByEmailController } from "./useCases/GetUserByEmail"
+import { loginController } from "./useCases/Login"
 import { updateUserController } from "./useCases/UpdateUser"
 
 const router = Router()
+
+/* CRUD User */
 
 router.post('/user', (request, response) => {
     return createUserController.handle(request, response)
@@ -24,6 +27,12 @@ router.put('/user/:email', (request, response) => {
 
 router.delete('/user/:id', (request, response) => {
     return deleteUserController.handler(request, response)
+})
+
+/* Auth User */
+
+router.post('/user/auth', (request, response)=> {
+    return loginController.handle(request, response)
 })
 
 export { router }
