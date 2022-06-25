d25d2763e43e037826473ced4dc77ec155880801
gerssivaldosantos <canalandows@gmail.com>
2022-06-25 20:57:20 -0300

    refactor: remove unecessary code

--- src/databases/typeorm/data-source.ts
+++ src/databases/typeorm/data-source.ts
@@ -5,10 +5,4 @@ import ormConfig from "../../../ormconfig.json"
 
 const AppDataSource = new DataSource(ormConfig as DataSourceOptions)
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Database connected")
-    })
-    .catch((error) => console.log(error))
-
 export {AppDataSource}
