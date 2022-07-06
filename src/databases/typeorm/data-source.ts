be08b0c05a3405a35a440e9ea84d0fde8ac6d6f5
gerssivaldosantos <canalandows@gmail.com>
2022-07-05 22:08:30 -0300

    refactor: remove test

--- src/databases/typeorm/data-source.ts
+++ src/databases/typeorm/data-source.ts
@@ -4,5 +4,5 @@ import { DataSource, DataSourceOptions } from "typeorm"
 import ormConfig from "../../../ormconfig.json"
 
 const AppDataSource = new DataSource(ormConfig as DataSourceOptions)
-
+AppDataSource.initialize()
 export {AppDataSource}
