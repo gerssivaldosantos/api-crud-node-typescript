b2241d98c79b5af0c8b548ce8bcd3479d95c2911
gerssivaldosantos <canalandows@gmail.com>
2022-07-13 00:44:46 -0300

    feat: add crud actions

--- src/repositories/implementations/PostgresUserRepository.ts
+++ src/repositories/implementations/PostgresUserRepository.ts
@@ -17,7 +17,9 @@ class PostgresUserRepository implements UserRepositoryInterface {
         throw new Error("Method not implemented.");
     }
     async findById(id: string): Promise<User> {
-        throw new Error("Method not implemented.");
+        const user = await this.client.query(`SELECT * FROM users WHERE id = '${id}'`);
+        delete user.rows[0].password;
+        return user.rows[0];
     }
     async findByEmail(email: string): Promise<User> {
         const user = await this.client.query(`SELECT * FROM users WHERE email = '${email}'`);
@@ -25,16 +27,20 @@ class PostgresUserRepository implements UserRepositoryInterface {
         return user.rows[0];
     }
     async customFindByEmail(email: string): Promise<User> {
-        throw new Error("Method not implemented.");
+        const user = await this.client.query(`SELECT * FROM users WHERE email = '${email}'`);
+        return user.rows[0];
     }
     async findAll(): Promise<User[]> {
-        throw new Error("Method not implemented.");
+        const users = await this.client.query(`SELECT * FROM users`);
+        delete users.rows[0].password;
+        return users.rows;
     }
     async update(user: User): Promise<void> {
-        throw new Error("Method not implemented.");
+        const { id, name, email, password } = user;
+        await this.client.query(`UPDATE users SET name = '${name}', email = '${email}', password = '${password}' WHERE id = '${id}'`);
     }
     async delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.client.query(`DELETE FROM users WHERE id = '${id}'`);
     }
 
 }
