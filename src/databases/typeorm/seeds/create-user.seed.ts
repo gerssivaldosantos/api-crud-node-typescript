b114d0c356cd57afb2ed7e0b7512b43807641084
gerssivaldosantos <canalandows@gmail.com>
2022-07-24 11:42:48 -0300

    refactor: new organization to folder structure

---
+++ src/databases/typeorm/seeds/create-user.seed.ts
@@ -0,0 +1,18 @@
+import { Factory, Seeder } from 'typeorm-seeding'
+import { User } from '../../../entities/User'
+import { DataSource } from 'typeorm'
+import { faker } from '@faker-js/faker'
+
+export default class createUser implements Seeder {
+  public async run(_factory: Factory, AppDataSource: DataSource): Promise<any> {
+    await AppDataSource.initialize().then(dataSource => {
+      const user = new User({
+        name: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: "123456",
+      });
+      dataSource.getRepository(User).save(user);
+    })
+
+  }
+}
