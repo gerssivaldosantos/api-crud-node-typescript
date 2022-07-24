2dfe6dbd147557371a32a972eca84cfbdd3aa1a1
gerssivaldosantos <canalandows@gmail.com>
2022-07-24 11:13:08 -0300

    feat: add create profile test spec

---
+++ src/useCases/profile/CreatePerfil/create-profile.spec.ts
@@ -0,0 +1,28 @@
+import { faker } from "@faker-js/faker";
+import request from 'supertest'
+import app from "../../../app";
+import { AppDataSource } from "../../../databases/typeorm/data-source";
+import { Profile } from "../../../entities/Profile";
+
+beforeAll(async () => {
+    await AppDataSource.initialize();
+    await AppDataSource.runMigrations()
+} )
+
+describe("Create profile", () => {
+    it('Should be able to create a new profile with success', async () => {
+        const profile = new Profile({
+            name: faker.name.jobType(),
+            description: faker.name.jobDescriptor(),
+        })
+        const response = await request(app).post('/profile').send(profile)
+        const { statusCode, body } = response
+        expect(statusCode).toEqual(201)
+        expect(body).toHaveProperty('message')
+        expect(body.message).toEqual('Profile Created')
+    })
+})
+
+afterAll( done => {
+    AppDataSource.destroy().then(() => done());
+})
