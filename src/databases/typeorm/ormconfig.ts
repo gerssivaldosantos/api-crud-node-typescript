2d9dde1aed255efdc0b7bd5903f04b0509d7cea4
gerssivaldosantos <canalandows@gmail.com>
2022-08-25 22:27:45 -0300

    feat: add default orm config

---
+++ src/databases/typeorm/ormconfig.ts
@@ -0,0 +1,26 @@
+export default {
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+    logging: true,
+    synchronize: true,
+    entities: [
+        "entity/*.js"
+    ],
+    subscribers: [
+        "subscriber/*.js"
+    ],
+    entitySchemas: [
+        "schema/*.json"
+    ],
+    migrations: [
+        "migration/*.js"
+    ],
+    cli: {
+        entitiesDir: "entity",
+        migrationsDir: "migration",
+        subscribersDir: "subscriber"
+    }
+}
