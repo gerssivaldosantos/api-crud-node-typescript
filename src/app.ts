f68cac0e7c35c7f61d2a93e53d6f011fb7c6450f
gerssivaldosantos <canalandows@gmail.com>
2022-08-28 14:06:51 -0300

    feat: add prefix for all routes

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ class MyApp {
         this.app.use(express.json());
         this.app.use(cors());
         this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-        this.app.use(router)
+        this.app.use('/api/v1/', router)
     }
 }
 
