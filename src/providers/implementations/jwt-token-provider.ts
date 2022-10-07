c204bddd33b77fd701181961398948a0064718c0
gerssivaldosantos <canalandows@gmail.com>
2022-10-06 21:50:19 -0300

    feat: add get_info

--- src/providers/implementations/jwt-token-provider.ts
+++ src/providers/implementations/jwt-token-provider.ts
@@ -1,5 +1,6 @@
 import { TokenProviderInterface } from "../token-provider.interface";
 import jwt from "jsonwebtoken";
+import {TokenPayload} from "@/generic/dtos/token-payload.dto";
 
 export class JwtTokenProvider implements TokenProviderInterface {
     constructor() { }
@@ -8,7 +9,29 @@ export class JwtTokenProvider implements TokenProviderInterface {
         return jwt.sign({ id: id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
     }
 
-    check(token: string): Promise<undefined> {
-        return Promise.resolve();
+    check(token: string): boolean {
+            if (!token) throw new Error('Token not provided')
+            try {
+                jwt.verify
+                (
+                    token.replace('Bearer', '').trim(),
+                    process.env.JWT_SECRET_KEY
+                )
+            } catch (err) {
+                console.error(err)
+            }
+            return true
+ }
+
+    get_info(token: string): { id: string; exp: number; iat: number } {
+        if (!token) throw new Error('Token not provided')
+
+        const data = jwt.verify
+        (
+            token.replace('Bearer', '').trim(),
+            process.env.JWT_SECRET_KEY
+        )
+        const { id, iat, exp } = data as TokenPayload
+        return {id, iat, exp}
     }
 }
--- src/providers/token-provider.interface.ts
+++ src/providers/token-provider.interface.ts
@@ -1,5 +1,6 @@
 export interface TokenProviderInterface{
     create(id: string): Promise<string>
+    check(token: string): boolean
+    get_info(token: string): { id: string; exp: number; iat: number }
 
-    check(token: string): Promise<undefined>
 }
